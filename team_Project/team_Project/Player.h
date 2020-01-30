d4572ea54d8bdf5e9da8dded231813d52694ed97
KuonAbe <60372806+KuonAbe@users.noreply.github.com>
2020-01-30 16:01:10 +0900

    マップチップの描画ができるよ♡

--- team_Project/team_Project/Player.h
+++ team_Project/team_Project/Player.h
@@ -1,15 +1,30 @@
 #pragma once
 #include<string>
 
-class 
+#define PLAYER_SIZE 32
+
+class Player
 {
 	using string = std::string;
 
 private:
 
 	//プレイヤー用の変数
-	
+	int pos_X,//プレイヤーのポジションX
+		pos_Y,//プレイヤーのポジションY
+		Cr,//プレイヤーの色(デモ用)
+		velocity_X,//プレイヤーの移動量X
+		velocity_Y;//プレイヤーの移動量Y
+
+	bool is_Hit,//プレイヤーのヒットフラグ
+		 is_Dead;//プレイヤーの死亡フラグ
 
 public:
+
+	void Init();//プレイヤー用の初期化用
+
+	void Move();//プレイヤーの移動用
+
+	void Draw();//プレイヤーの描画用
 
 };
--- team_Project/team_Project/Title.h
+++ team_Project/team_Project/Title.h
@@ -1,1 +1,14 @@
 #pragma once
+#include<string>
+
+class Title
+{
+private:
+
+public:
+
+	void Title_Init();
+
+	void Title_Draw();
+
+};
