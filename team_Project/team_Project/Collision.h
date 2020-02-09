f1d2f7875e9ffbed1eeef35a88b7dc38f66ab015
KuonAbe <60372806+KuonAbe@users.noreply.github.com>
2020-02-09 13:39:31 +0900

    当たり判定がお菓子です。早くここ抜け出したいです

--- team_Project/team_Project/Collision.h
+++ team_Project/team_Project/Collision.h
@@ -6,10 +6,25 @@ class Collision
 private:
 
 public:
-	bool Block_Collision(int Block_PosX,//ブロックの左上のX
-						 int Block_PosY,//ブロックの左上のY
-						 int Player_PosX,//プレイヤーの左上のX
-						 int Player_posY);//プレイヤーの左上のY
+	bool Block_Collision_Up(int Block_PosX,//ブロックの左上のX
+		int Block_PosY,//ブロックの左上のY
+		int Player_PosX,//プレイヤーの左上のX
+		int Player_posY);//プレイヤーの左上のY
+
+	bool Block_Collision_Left(int Block_PosX,//ブロックの左上のX
+		int Block_PosY,//ブロックの左上のY
+		int Player_PosX,//プレイヤーの左上のX
+		int Player_posY);//プレイヤーの左上のY
+
+	bool Block_Collision_Right(int Block_PosX,//ブロックの左上のX
+		int Block_PosY,//ブロックの左上のY
+		int Player_PosX,//プレイヤーの左上のX
+		int Player_posY);//プレイヤーの左上のY
+
+	bool Block_Collision_Bottom(int Block_PosX,//ブロックの左上のX
+		int Block_PosY,//ブロックの左上のY
+		int Player_PosX,//プレイヤーの左上のX
+		int Player_posY);//プレイヤーの左上のY
 
 	bool Item_Collision();//鍵などのアイテムの当たり判定のメソッド
 };
