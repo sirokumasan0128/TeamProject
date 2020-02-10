627c70c91f34fea1963aff1ec04e0d5c4ea8c0af
sirokumasan <sirokumasan0128@ezweb.ne.jp>
2020-02-10 16:21:38 +0900

    プッシュしない

--- team_Project/team_Project/Collision.cpp
+++ team_Project/team_Project/Collision.cpp
@@ -54,7 +54,7 @@ bool Collision::Block_Collision_Left(int Block_PosX, int Block_PosY,
 	int vectlx, vectly;
 
 	if (std::abs(Player_PosX - Block_PosX) < 32 / 2 + 32 / 2 &&
-		std::abs(Player_PosY - Block_PosY) < 32 / 2 + 32 / 2)
+		std::abs(Player_PosY  - Block_PosY) < 32 / 2 + 32 / 2)
 	{
 		vectlx = Player_PosX - Block_PosX;
 		vectly = Player_PosY - Block_PosY;
--- team_Project/team_Project/Stage1_2.cpp
+++ team_Project/team_Project/Stage1_2.cpp
@@ -6,6 +6,7 @@
 //#include"Player.h"
 //#include"Collision.h"
 
+
 using namespace std;
 
 Stage1_2::Stage1_2()
@@ -23,16 +24,16 @@ void Stage1_2::Init()
 
 void Stage1_2::Update()
 {
-
+	player.Update();
 }
 
 void Stage1_2::Draw()
 {
 
 
-	for (int h = 0;h < MAP_HEIGHT;h++)
+	for (int h = 0; h < MAP_HEIGHT; h++)
 	{
-		for (int w = 0;w < MAP_WIDTH;w++)
+		for (int w = 0; w < MAP_WIDTH; w++)
 		{
 			vector<int> Block_Position = { w*BLOCK_SIZE,h*BLOCK_SIZE };
 
@@ -51,17 +52,66 @@ void Stage1_2::Draw()
 
 void Stage1_2::Stage1_2_Map_Hit()
 {
-	player.Update();
-	for (int h = 0;h < MAP_HEIGHT;h++)
+	//ここで二重呼び出しになっている説
+	//player.Update();
+	//ここのupdataを切るとこっちのクラスで値を取得できない
+
+
+	//値を持ってこれてるか確認用
+	DrawFormatString(400, 400, GetColor(255, 255, 255), "stageclass  %d\n%d", player.GetPlayerPosX(), player.GetPlayerPosY());
+
+
+	for (int h = 0; h < MAP_HEIGHT; h++)
 	{
-		for (int w = 0;w < MAP_WIDTH;w++)
+		for (int w = 0; w < MAP_WIDTH; w++)
 		{
 			//vector<int> Block_Position = { w*BLOCK_SIZE,h*BLOCK_SIZE };
 
-			//値を持ってこれてるか確認用
-			DrawFormatString(400, 400, GetColor(255, 255, 255), "%d\n%d", player.GetPlayerPosX(), player.GetPlayerPosY());
+
+
 			//緑のブロックとの当たり判定
 			//ここでブロックの上とプレイヤーの当たり判定を見てる
+
+
+
+			//値を持ってこれてるか確認用
+			DrawFormatString(400, 400, GetColor(255, 255, 255), "%d\n%d", player.GetPlayerPosX(), player.GetPlayerPosY());
+
+
+
+			//緑のブロックとの当たり判定
+			//ここでブロックの上とプレイヤーの当たり判定を見てる
+			/*if (collision.Block_Collision_Right(
+				w*BLOCK_SIZE, h*BLOCK_SIZE,
+				player.GetPlayerPosX(),
+				player.GetPlayerPosY()) == true &&
+				Stage1_MapInfo_Layer2[h][w] != 000)
+
+			{
+				if (collision.Block_Collision_Up (
+					w*BLOCK_SIZE, h*BLOCK_SIZE,
+					player.GetPlayerPosX(),
+					player.GetPlayerPosY()) == true)
+				{
+
+					player.SetPlayerVelocityY(0);
+
+					player.SetPlayerPosX(500);
+					player.SetPlayerPosY(500);
+
+
+					//player.SetPlayerVelocityX(0);
+
+					//DrawFormatString(600, 400, GetColor(255, 255, 255), "%d\n%d", player.GetPlayerPosX(), player.GetPlayerPosY());
+					DrawFormatString(600, 460, GetColor(255, 255, 255), "%d\n%d", w*BLOCK_SIZE, h*BLOCK_SIZE);
+				}
+
+
+
+
+			}*/
+
+
 			if (collision.Block_Collision_Up(
 				w*BLOCK_SIZE, h*BLOCK_SIZE,
 				player.GetPlayerPosX(),
@@ -69,9 +119,60 @@ void Stage1_2::Stage1_2_Map_Hit()
 				Stage1_MapInfo_Layer2[h][w] != 000)
 			{
 
-				player.SetPlayerVelocityY(0);
 
+				player.SetPlayerPosY(h*BLOCK_SIZE - 32);
+
+				//player.SetPlayerVelocityY(0);
+				DrawFormatString(500, 500, GetColor(255, 255, 255), "%d", player.GetPlayerVelocityY());
+
+
+
+				//player.SetPlayerVelocityX(0);
+
+				//DrawFormatString(600, 400, GetColor(255, 255, 255), "%d\n%d", player.GetPlayerPosX(), player.GetPlayerPosY());
+				DrawFormatString(600, 460, GetColor(255, 255, 255), "%d\n%d", w*BLOCK_SIZE, h*BLOCK_SIZE);
 			}
+
+
+			if (collision.Block_Collision_Bottom(w*BLOCK_SIZE, h*BLOCK_SIZE,
+				player.GetPlayerPosX(),
+				player.GetPlayerPosY()) == true &&
+				Stage1_MapInfo_Layer2[h][w] != 000)
+			{
+
+				//下から
+				player.SetPlayerPosY(h*BLOCK_SIZE + 32);
+			}
+
+			if (collision.Block_Collision_Right(w*BLOCK_SIZE, h*BLOCK_SIZE,
+				player.GetPlayerPosX(),
+				player.GetPlayerPosY()) == true &&
+				Stage1_MapInfo_Layer2[h][w] != 000)
+			{
+
+
+				player.SetPlayerPosX(w*BLOCK_SIZE);
+			}
+
+			if (collision.Block_Collision_Left(w*BLOCK_SIZE, h*BLOCK_SIZE,
+				player.GetPlayerPosX(),
+				player.GetPlayerPosY()) == true &&
+				Stage1_MapInfo_Layer2[h][w] != 000)
+			{
+
+
+				player.SetPlayerPosX(w*BLOCK_SIZE - 32);
+			}
+
+
+			/*if (Stage1_MapInfo_Layer2[h][w] == 002 )
+			{
+
+
+
+
+
+			}*/
 
 
 		}
